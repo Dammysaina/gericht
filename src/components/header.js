2c89b9138b9096ac099a1e2c463853f35eeb3e77
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-04-13 19:34:25 +0100

    header

---
+++ src/components/header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Glogo from "../assets/images/Geríchtlogo.svg";
+import "../assets/css/header.css";
+
+const Header = () => {
+  return (
+    <div className="header_container">
+      <div className="header_contain">
+        <div>
+          <img src={Glogo} alt="Gerichtlogo" />
+        </div>
+        <div className="home_list">
+          <ul>
+            <li>Home</li>
+            <li>Pages</li>
+            <li>Contact Us</li>
+            <li>Blog</li>
+            <li>Landing</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Log in / registration</li>
+            <li></li>
+            <li>Book table</li>
+          </ul>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
