adb3fcc9d8e673a5c9d2d09c7deac968e734e8e9
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-04-18 19:38:59 +0100

    history

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,33 +1,43 @@
 import React from "react";
 import spoon from "../assets/images/spoon.svg";
 import CustomButton from "../library/CustomButton";
+import "../assets/css/aboutus.css";
+import knife from "../assets/images/knife.svg";
+// import gbackground from "../assets/images/G.svg";
 
 const Aboutus = () => {
   return (
     <div>
       <div className="aboutus_container">
-        <div>
-          <p>About Us</p>
-          <img src={spoon} alt="spoonimage" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
-            pharetra adipiscing ultrices vulputate posuere tristique. In sed
-            odio nec aliquet eu proin mauris et.
-          </p>
-          <CustomButton>Know More</CustomButton>
-        </div>
-        <div>
-            
-        </div>
-        <div>
-          <p>About Us</p>
-          <img src={spoon} alt="spoonimage" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
-            pharetra adipiscing ultrices vulputate posuere tristique. In sed
-            odio nec aliquet eu proin mauris et.
-          </p>
-          <CustomButton>Know More</CustomButton>
+        <div className="g_background">
+          <div className="our_history">
+            <div className="text-right">
+              <p className="bg-10 new_flavour bg-yellow ">About Us</p>
+              <img src={spoon} alt="spoonimage" />
+              <p className="flavour_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+                pharetra adipiscing ultrices vulputate posuere tristique. In sed
+                odio nec aliquet eu proin mauris et.
+              </p>
+              <CustomButton>Know More</CustomButton>
+            </div>
+
+            <div>
+              {/* <img src={gbackground} alt="gbackground" /> */}
+
+              <img src={knife} alt="knife" />
+            </div>
+            <div>
+              <p className="bg-10 new_flavour bg-yellow">About Us</p>
+              <img src={spoon} alt="spoonimage" />
+              <p className="flavour_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+                pharetra adipiscing ultrices vulputate posuere tristique. In sed
+                odio nec aliquet eu proin mauris et.
+              </p>
+              <CustomButton>Know More</CustomButton>
+            </div>
+          </div>
         </div>
       </div>
     </div>
