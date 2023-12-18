095d9d72aa146ce4a33d43333c6b14c7fc680766
dammysaina <dammyadediran94@gmail.com>
2023-12-18 18:41:35 +0100

    signature

--- src/components/chefsword.js
+++ src/components/chefsword.js
@@ -1,28 +1,36 @@
 import React from "react";
 import spoon from "../assets/images/spoon.svg";
 import lettuceman from "../assets/images/lettuceman.svg";
-import "../assets/css/aboutus.css"
+import "../assets/css/aboutus.css";
 
 const Chef = () => {
-    return (
-        <div className="chefsword">
-            <div>
-            <img src={lettuceman} alt=""/>
-            </div>
+  return (
+    <div className="chefsword">
+      <div>
+        <img src={lettuceman} alt="" />
+      </div>
 
-            <div>
-                <div>
-                    <p className=" new_flavour">Chef's Word</p>
-                    <img src={spoon} alt="spoonimage"/>
-                </div>
-                <div>
-                    <p className="bg-yellow new_flavour">What we believe in</p>
-                    <p><span>&amps;</span></p>
-                </div>
-            </div>
+      <div>
+        <div>
+          <p className=" new_flavour">Chef's Word</p>
+          <img src={spoon} alt="spoonimage" />
         </div>
-    )
-}
-
+        <div>
+          <p className="bg-yellow new_flavour">What we believe in</p>
+          <p className="flavour_text">
+            <span className="">&apos;&apos;</span>Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit auctor sit. auctor sit iaculis in arcu.
+            Vulputate nulla lobortis mauris eget sit. Nulla scelerisque
+            scelerisque congue ac consequat, aliquam molestie lectus eu. Congue
+            iaculis integer curabitur semper sit nunc.
+          </p>
+          <p className="chefsname">Kevin Luo</p>
+          <p>Chef & Founder</p>
+          <p className="signature">Kevin Luo</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Chef;
